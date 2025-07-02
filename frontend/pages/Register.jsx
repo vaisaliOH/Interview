880bcebbeda3bb0afe96a5d2767f3a7f1b4441f9
vaisaliOH <vaisalinair05@gmail.com>
2025-07-02 21:06:10 +0530

    third commit

--- frontend/pages/Register.jsx
+++ frontend/pages/Register.jsx
@@ -10,7 +10,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch('https://interview-d45g.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
